d5f0af0ed6d5f13e2c31602cbc5d7ef86688ef05
Carlos Eduardo Cruz <121514453+EduardoCR91@users.noreply.github.com>
2025-09-04 02:20:02 +0000

    pagian y estilos

--- src/Disney.jsx
+++ src/Disney.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import EduardoCruz from './EduardoCruz.jsx'
 
 function Disney() {
 
   return (
     <>
       ahhh  d
+      <EduardoCruz/>
     </>
   )
 }
