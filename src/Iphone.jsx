bf4e88a96c711078faadb7ce5fa24c43bfef00e4
Julianv2903 <oscar.villamarinw@uniagustiniana.edu.co>
2025-09-04 02:20:02 +0000

    terminos y condiciones Este apartado contiene infromacion acerca de terminos y condiciones de celulares de line apple

--- src/Iphone.jsx
+++ src/Iphone.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Villamarin_terminos_condiciones from './Villamarin_terminos_condiciones.jsx'
 
 function Iphone() {
 
   return (
     <>
       ahhh i
+      <Villamarin_terminos_condiciones/>
     </>
   )
 }
