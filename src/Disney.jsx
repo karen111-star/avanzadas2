d9f9e93f2335d7dce6ee791a03d385b01266493e
cristiav <Cristian.vegag@uniagustiniana.edu.co>
2025-09-04 02:21:17 +0000

    Vega

--- src/Disney.jsx
+++ src/Disney.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Vega_seccion from './Vega_seccion.jsx'
 
 function Disney() {
 
   return (
     <>
       ahhh  d
+      <Vega_seccion/>
     </>
   )
 }
