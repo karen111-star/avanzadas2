f1b691b10b49ea82c3b0343a2a448f72777b2b75
Juanu79 <j.estebanurriago@gmail.com>
2025-09-04 02:19:46 +0000

    Urriago_Seccion

--- src/Disney.jsx
+++ src/Disney.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import App from './Urriago.jsx'
+import Urriago from './Urriago.jsx'
+
 
 function Disney() {
 
   return (
     <>
       ahhh  d
+      <Urriago/>
     </>
   )
 }
