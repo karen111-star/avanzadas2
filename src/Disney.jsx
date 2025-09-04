01c0e8db93288312bbfa0426419a7b1404be6bdc
dani1209 <daniexlasanchez@gmail.com>
2025-09-04 02:20:37 +0000

    Paula Parra Paula Parra

--- src/Disney.jsx
+++ src/Disney.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-
+import Parra_seccion from './Parra_seccion.jsx'
 function Disney() {
 
   return (
     <>
       ahhh  d
+      <Parra_seccion/>
     </>
   )
 }
