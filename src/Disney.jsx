53751d14567b47eb3ca81049bba04ac1314116d5
juanse68 <163223814+juanse68@users.noreply.github.com>
2025-09-04 02:19:53 +0000

    cambios

--- src/Disney.jsx
+++ src/Disney.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Piratoba from './Piratoba.jsx'
 
 function Disney() {
 
   return (
     <>
       ahhh  d
+      <Piratoba/>
     </>
   )
 }
 
-export default Disney
+export default Piratoba
