e547fc7d0e7941e1d1a022e0d91ad1065d41345d
Juan-Caicedo27 <juan.caicedoc@uniagustiniana.edu.co>
2025-09-04 02:18:47 +0000

    Cambios juan caicedo

--- src/Iphone.jsx
+++ src/Iphone.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Caicedo_encabezado from './Caicedo.jsx'  
 
 function Iphone() {
 
   return (
     <>
       ahhh i
+      <Caicedo_encabezado/>
     </>
   )
 }
 
-export default Iphone
+export default Iphone;
