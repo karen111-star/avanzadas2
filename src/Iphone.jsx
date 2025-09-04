3d7e6c69ac5fa1e597c93a845731f9ee315284d1
Odinlowe1651 <odinlowe1651@gmail.com>
2025-09-04 02:17:50 +0000

    ch1

--- src/Iphone.jsx
+++ src/Iphone.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-
+import Rubio_Alejandro_Iphone from './Rubio_Alejandro_Iphone.jsx'
 function Iphone() {
 
   return (
     <>
       ahhh i
+      <Rubio_Alejandro_Iphone/>
     </>
   )
 }
