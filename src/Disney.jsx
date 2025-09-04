4eba661b468d73742db9d16e9cf49f92c1b52776
Santiagocd1902 <david.castellanosd@uniagustiniana.edu.co>
2025-09-04 02:19:36 +0000

    Castellanos Disney

--- src/Disney.jsx
+++ src/Disney.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Castellanos from './Castellanos.jsx'
 
 function Disney() {
 
   return (
     <>
       ahhh  d
+      <Castellanos/>
     </>
   )
 }
