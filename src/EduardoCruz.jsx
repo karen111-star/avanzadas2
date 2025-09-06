8d2b610264687d31ca43fc834f117fdc9cbcd8bc
Carlos Eduardo Cruz <121514453+EduardoCR91@users.noreply.github.com>
2025-09-06 20:14:19 +0000

    solicud de PR

--- src/EduardoCruz.jsx
+++ src/EduardoCruz.jsx
@@ -6,6 +6,7 @@ import './EduardoCruz.css'
 // To resolve the file path issue, the CSS is included directly within the component.
 
 // --- SVG Icons as Components ---
+//Comentaerio para mandar un PR
 const ProfileIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
     <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
